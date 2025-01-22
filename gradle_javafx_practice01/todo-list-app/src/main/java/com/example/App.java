184577a68baa5b35806b9982c3864ac838e907ea
andgamespace <anshxc@gmail.com>
2025-01-22 14:23:22 -0600

    .

--- gradle_javafx_practice01/todo-list-app/src/main/java/com/example/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        // Initialize the application components
-        System.out.println("Welcome to the Todo List Application!");
-        
-        // Setup necessary components here (e.g., services, user interface)
-    }
-}
